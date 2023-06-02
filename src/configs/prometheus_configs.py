f3014b39b1b64ff9d65ae3c1cac25eb179e7a308
brunohaf <bruno3145@gmail.com>
2023-06-02 18:07:15 -0300

    adds: query and log template configs

--- src/configs/prometheus_configs.py
+++ src/configs/prometheus_configs.py
@@ -1,4 +1,11 @@
 class PrometheusConfigs:
     def __init__(self, json_config: dict) -> None:
         self.server_url = json_config['prometheus_server_url']
-        self.requests_query = json_config['requests_query']
+        self.query = QueryConfigs(json_config['query'])
+
+class QueryConfigs:
+    def __init__(self, json_config: dict) -> None:
+        self.time_range = json_config['time_range']
+        self.component = json_config['component']
+        self.template = json_config['template']
+        self.metric = json_config['metric']
--- src/configs/seq_configs.py
+++ src/configs/seq_configs.py
@@ -1,5 +1,11 @@
 class SeqConfigs:
     def __init__(self, json_config: dict) -> None:
-        self.batch_size = json_config['batch_size']
+        self.templates = LogTemplates(json_config['log_templates'])
         self.server_url = json_config['server_url']
         self.api_key = json_config['api_key']
+        self.port = json_config['port']
+
+class LogTemplates:
+    def __init__(self, json_config: dict) -> None:
+        self.purging_message = json_config['purging_message']
+        self.error_message = json_config['error_message']
