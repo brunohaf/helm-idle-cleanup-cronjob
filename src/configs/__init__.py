7ed198a7939ca3b967a6a0a1650dce06238532ec
brunohaf <bruno3145@gmail.com>
2023-06-07 16:19:14 -0300

    adds: azure file share configs

--- src/configs/__init__.py
+++ src/configs/__init__.py
@@ -2,3 +2,4 @@ from .configs import Configs
 from .seq_configs import SeqConfigs, LogTemplates
 from .prometheus_configs import PrometheusConfigs, QueryConfigs
 from .cronjob_configs import CronjobConfigs
+from .azure_fileshare_configs import AzureFileShareConfigs
--- src/configs/configs.py
+++ src/configs/configs.py
@@ -1,12 +1,14 @@
 from .seq_configs import SeqConfigs
 from .prometheus_configs import PrometheusConfigs
 from .cronjob_configs import CronjobConfigs
+from .azure_fileshare_configs import AzureFileShareConfigs
 from json import load
 
 class Configs():
     def __init__(self):
         with open('./src/configs/configs.json', 'r') as configs:
             configs = load(configs)
+            self.azure_fileshare = AzureFileShareConfigs(configs['azure_fileshare'])
             self.prometheus = PrometheusConfigs(configs['prometheus'])
             self.cronjob = CronjobConfigs(configs['cronjob'])
             self.seq = SeqConfigs(configs['seq'])
