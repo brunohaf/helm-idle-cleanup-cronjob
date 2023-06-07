41274eb382797a3d54cd9c06641b4557b22d436a
brunohaf <bruno3145@gmail.com>
2023-06-07 16:21:43 -0300

    adds: file share step to cronjob script

--- src/helm_idle_cleaner.py
+++ src/helm_idle_cleaner.py
@@ -1,21 +1,24 @@
 from configs import Configs
 from services import PrometheusClient
 from kubernetes import client, config as k8s_config
+from azure.storage.fileshare import ShareFileClient, ShareDirectoryClient
+from datetime import datetime
 from pygelf import GelfUdpHandler
+import json
 import logging
 
-# Configuration
+# Configuration.
 configs = Configs()
 
-# Kubernetes Settings (use k8s_config.load_kube_config() locally)
+# Kubernetes Settings (use k8s_config.load_kube_config() locally).
 k8s_config.load_incluster_config()
 k8s_client_instance = client.AppsV1Api(k8s_config.new_client_from_config())
 
-# Prometheus Settings
+# Prometheus Settings.
 query_configs = configs.prometheus.query
 prometheus = PrometheusClient(url=configs.prometheus.server_url)
 
-#Seq Settings
+#Seq Settings.
 logging_configs = configs.seq
 logging_templates = logging_configs.templates
 logger = logging.getLogger()
@@ -23,7 +26,12 @@ logger.addHandler(GelfUdpHandler(
     host=logging_configs.server_url,
     port=logging_configs.port))
 
-# List elegible deployments
+# Azure File Share Settings.
+file_share_configs = configs.azure_fileshare
+today = datetime.now().strftime(file_share_configs.filename_date_format)
+file_name = file_share_configs.filename_template.format(file_name=today)
+
+# List elegible deployments.
 allowed_list_resp = k8s_client_instance.list_namespaced_deployment(
     namespace=configs.cronjob.namespace,
     label_selector=f'team in ({",".join(configs.cronjob.teams)})')
@@ -34,7 +42,7 @@ allowed_list = list(map(
         "team": deployment.metadata.labels.get("team")
         }, allowed_list_resp.items))
 
-# Get from Prometheus the list of ingress with requests in the last 2 weeks
+# Get from Prometheus the list of ingress with requests in the last 2 weeks.
 requests_query = query_configs.template.format(
     metric=query_configs.metric,
     component=query_configs.component,
@@ -43,6 +51,7 @@ requests_query = query_configs.template.format(
 query_result = prometheus.custom_query(requests_query)
 to_be_purged = []
 
+# List releases to be purged.
 for ingress in query_result:
     try:
         metric = ingress.get('metric', None)
@@ -62,4 +71,10 @@ for ingress in query_result:
     finally:
         continue
 
-go = f"save_on_blob_storage({to_be_purged})"
+# Uploads releases list to be purged on Azure File Share.
+azure_file_client = ShareFileClient.from_connection_string(
+    conn_str=file_share_configs.conn_str,
+    share_name=file_share_configs.share_name,
+    file_path=file_name)
+
+azure_file_client.upload_file(json.dumps(to_be_purged))
