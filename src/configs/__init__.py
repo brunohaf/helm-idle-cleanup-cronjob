ee14e7f2eeceb1e60bdb40a64357248ccdd39056
brunohaf <31559311+brunohaf@users.noreply.github.com>
2023-09-28 15:27:48 -0300

    adds: azure fileshare dependencies on __init__.py

--- src/configs/__init__.py
+++ src/configs/__init__.py
@@ -1,4 +1,5 @@
 from .configs import Configs
-from .seq_configs import SeqConfigs
-from .prometheus_configs import PrometheusConfigs
+from .seq_configs import SeqConfigs, LogTemplates
+from .prometheus_configs import PrometheusConfigs, QueryConfigs
 from .cronjob_configs import CronjobConfigs
+from .azure_fileshare_configs import AzureFileShareConfigs
